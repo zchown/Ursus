9327a42169db8b0bf031dc7510f71cea1c484d58
zchown <91749140+zchown@users.noreply.github.com>
2026-07-08 20:48:50 -0700

    bench 5066850

--- src/engine/search.zig
+++ src/engine/search.zig
@@ -1136,7 +1136,7 @@ pub const Searcher = struct {
             hist.updateCaptureHistory(self, board, color, best_move, &other_moves, depth);
         }
 
-        if (!skip_quiet and self.excluded_moves[self.ply].toU32() == 0) {
+        if (self.excluded_moves[self.ply].toU32() == 0) {
             var tt_flag = tt.EstimationType.Over;
             if (best_score >= beta) {
                 tt_flag = tt.EstimationType.Under;
