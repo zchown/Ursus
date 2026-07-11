0bdfea5374d8ed9d91d45596726d490c42cce616
zchown <91749140+zchown@users.noreply.github.com>
2026-07-11 12:32:49 -0700

    bench 5167574

--- src/engine/search.zig
+++ src/engine/search.zig
@@ -1136,11 +1136,11 @@ pub const Searcher = struct {
             hist.updateCaptureHistory(self, board, color, best_move, &other_moves, depth);
         }
 
-       if (!skip_quiet and self.excluded_moves[self.ply].toU32() == 0) {
+       if (self.excluded_moves[self.ply].toU32() == 0) {
             var tt_flag = tt.EstimationType.Over;
             if (best_score >= beta) {
                 tt_flag = tt.EstimationType.Under;
-            } else if (alpha != alpha_) {
+            } else if (alpha != alpha_ and !skip_quiet) {
                 tt_flag = tt.EstimationType.Exact;
             } 
 
