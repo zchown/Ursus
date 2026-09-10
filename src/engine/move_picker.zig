990ae1e47f3bacea6e2f01f2e4052a77b0e3acea
zchown <91749140+zchown@users.noreply.github.com>
2026-09-10 16:38:44 -0700

    bench 5261816

--- src/engine/move_picker.zig
+++ src/engine/move_picker.zig
@@ -100,16 +100,26 @@ pub const MovePicker = struct {
         return p;
     }
 
-    pub fn initWithSeeThreshold(hash_move: mvs.EncodedMove, see_threshold: i32) MovePicker {
+    pub fn initWithSeeThreshold(hash_move: mvs.EncodedMove, is_null: bool, threshold: i32) MovePicker {
         var p = base(hash_move);
-        p.see_threshold = see_threshold;
+        p.is_null = is_null;
+        p.see_threshold = threshold;
         return p;
     }
+
 
     pub fn initNoisy(hash_move: mvs.EncodedMove) MovePicker {
         var p = base(hash_move);
         p.noisy_only = true;
         p.allow_quiet_tt = false;
+        return p;
+    }
+
+    pub fn initProbcut(hash_move: mvs.EncodedMove, see_threshold: i32) MovePicker {
+        var p = base(hash_move);
+        p.noisy_only = true;
+        p.allow_quiet_tt = true;
+        p.see_threshold = see_threshold;
         return p;
     }
 
--- src/engine/search.zig
+++ src/engine/search.zig
@@ -1009,7 +1009,7 @@ pub const Searcher = struct {
 
         if (cutnode and depth >= 6 and !in_check and beta < eval.mate_score - 256 and beta > -eval.mate_score + 256 and self.excluded_moves[self.ply].toU32() == 0) {
             const probcut_depth = depth - 3;
-            var pc_picker = mp.MovePicker.initWithSeeThreshold(hash_move, tp.probcut_min_see.value);
+            var pc_picker = mp.MovePicker.initProbcut(hash_move, tp.probcut_min_see.value);
             while (pc_picker.next(self, board)) |pc_picked| {
                 const move = pc_picked.move;
                 const see_score = pc_picked.see_val;
@@ -1095,7 +1095,7 @@ pub const Searcher = struct {
         var moves_seen: usize = 0;
 
         const noisy_threshold = if (on_pv or depth >= 8) tp.see_threshold.value else 0;
-        var picker = mp.MovePicker.initWithSeeThreshold(hash_move, noisy_threshold);
+        var picker = mp.MovePicker.initWithSeeThreshold(hash_move, is_null, noisy_threshold);
 
         while (picker.next(self, board)) |picked| {
             const move = picked.move;
