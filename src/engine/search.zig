4a90826eab19a62a705675f0529b787f0d5fe2e2
zchown <91749140+zchown@users.noreply.github.com>
2026-09-11 10:32:37 -0700

    bench 6138987

--- src/engine/search.zig
+++ src/engine/search.zig
@@ -1210,7 +1210,7 @@ pub const Searcher = struct {
 
                 if (s_score < s_beta) {
                     extension = 1;
-                    if (s_score < s_beta - tp.se_double_margin.value) {
+                    if (!cutnode and s_score < s_beta - tp.se_double_margin.value) {
                         extension = 2;
                     }
                 } else if (!on_pv and s_score >= beta and !eval.almostMate(s_score)) {
