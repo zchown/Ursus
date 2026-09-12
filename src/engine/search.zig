19daec5d89578428d07b84f9b34ed5ff48112ca8
zchown <91749140+zchown@users.noreply.github.com>
2026-09-12 16:13:06 -0700

    bench 5718406

--- src/engine/search.zig
+++ src/engine/search.zig
@@ -1029,15 +1029,8 @@ pub const Searcher = struct {
             if (null_threat.capture == 0) {
                 self.killer[self.ply + 1][0] = null_threat;
             } else {
-                board.makeNullMove();
-                if (see.seeCapture(board, self.move_gen, null_threat) < 0) {
-                    self.killer[self.ply + 1][0] = null_threat;
-                } else {
-                    // further boost cap history for null move threats
                     const dont_punish_others = mvs.MoveList.init(); 
                     hist.updateCaptureHistory(self, board, color.opposite(), null_threat, &dont_punish_others, depth);
-                }
-                board.unmakeNullMove();
             }
         }
 
