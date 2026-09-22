be95fea2d254b00656fad995aa0a75e9ec4a3eae
zchown <91749140+zchown@users.noreply.github.com>
2026-09-22 14:06:17 -0700

    bench 4933182

--- src/tunable_parameters.zig
+++ src/tunable_parameters.zig
@@ -76,8 +76,8 @@ pub var lmr_noisy_div = Tunable(i32, .{ .min = 150, .max = 600, .hook = .noisy_l
 
 pub var se_margin = Tunable(i32, .{ .min = 50, .max = 500}){ .value = 150};
 
-pub var history_div = Tunable(i32, .{ .min = 2048, .max = 8192 }){ .value = 6000 };
-pub var history_prune_mult = Tunable(i32, .{ .min = 512, .max = 8192 }){ .value = 2304 };
+pub var history_div = Tunable(i32, .{ .min = 2048, .max = 8192 }){ .value = 4098 };
+pub var history_prune_mult = Tunable(i32, .{ .min = 512, .max = 8192 }){ .value = 1536 };
 
 pub var stat_main_weight = Tunable(i32, .{ .min = 128, .max = 2048}){ .value = 1024};
 pub var stat_cont1_weight = Tunable(i32, .{ .min = 128, .max = 2048}){ .value = 512};
